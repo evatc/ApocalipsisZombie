d45a11f2c50e6cce8ee6e81712a71abde4217a67
evatc <eva.torresc@edu.uah.es>
2025-04-11 16:52:06 +0200

    empezado

--- src/Humano.java
+++ src/Humano.java
@@ -8,27 +8,20 @@ public class Humano extends Thread{
     private boolean herido = false;
     private boolean vivo = true;
 
-    public Humano(String id, Refugio refugio, Tunel tunel, Zona_riesgo zonaRiesgo){
+    public Humano(String id, Refugio refugio){
         this.id = id;
         this.refugio = refugio;
-        this.zonaRiesgo = zonaRiesgo;
     }
 
     public void run(){
-        Scanner sc = new Scanner(System.in);
-
         while (vivo) {
-            String texto = sc.nextLine();
-
-            if (texto.equals("salir")) {
-                break;
-            }
             double tiempo = (1 + Math.random())*1000;
-            refugio.zona_comun(this.id);
+            System.out.println("El humano " + this.id + " está en el área común");
             try{
                 Thread.sleep((long) tiempo);
             }catch(Exception e){}
             int n_tunel = (int) (Math.random()*4)+1;
+            System.out.println("El humano " + this.id + " ha elegido el tunel " + n_tunel);
             if(n_tunel == 1){
                 refugio.zona_espera_tunel1(this.id);
             }else if(n_tunel == 2){
@@ -42,10 +35,9 @@ public class Humano extends Thread{
             try{
                 Thread.sleep((long) tiempo1);
             }catch(Exception e){}
-            //aqui habria que hacer lo del ataque
             if(vivo){//si no le ha matado un zombie sigue con la rutina
                 if(!herido){ //si no ha sido atacado por un zombie pasa por los túneles(herido se va directamente a los tuneles sin pasar por aqui)
-                    zonaRiesgo.salir_humano(this.id,n_tunel);
+                    zonaRiesgo.salir_humano(this,n_tunel);
                     if(n_tunel == 1){
                         tunel.entrar1_zona_descanso(this.id);
                     }else if(n_tunel == 2){
@@ -55,21 +47,29 @@ public class Humano extends Thread{
                     }else{
                         tunel.entrar4_zona_descanso(this.id);
                     }
+                    System.out.println("El humano " + this.id + " ha recolectado 2 piezas de comida");
                     int comida = refugio.getComida();
                     refugio.setComida(comida + 2);
                 }
                 double tiempo2 = (2 + Math.random()*2)*1000;
+                System.out.println("El humano " + this.id + " está en el área de descanso");
                 try{
                     Thread.sleep((long) tiempo2);
                 }catch(Exception e){}
+                System.out.println("El humano " + this.id + " está en el comedor");
                 refugio.comedor(this.id);
-                //aqui iria lo de curarse q depende del ataque
-                System.out.println("Dijiste: " + texto);
+                if(herido){
+                    System.out.println("El humano " + this.id + " ha vuelto al área de descanso para curar sus heridas");
+                    double tiempo3 = (3 + Math.random()*2)*1000; // ns si ponerlo aquí o dentro de un método
+                    try{
+                        Thread.sleep((long) tiempo1);
+                    }catch(Exception e){}
+                }
             }
 
         }
 
-        sc.close();
+
     }
 
     public boolean isHerido() {
@@ -88,7 +88,6 @@ public class Humano extends Thread{
         this.vivo = vivo;
     }
 
-    @Override
     public String getId() {
         return id;
     }
